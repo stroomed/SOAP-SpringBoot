11a9d0f837be38f6d23f06db2363264864d22759
ricardo.gutierrez <ricardo.gutierrez.valejos@gmail.com>
2022-02-11 10:20:49 -0300

    App

---
+++ SOAP/src/main/java/com/ricardo/SOAP/CountryRepository.java
@@ -0,0 +1,25 @@
+package com.ricardo.SOAP;
+
+import com.baeldung.springsoap.gen.*;
+import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
+
+import javax.annotation.PostConstruct;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class CountryRepository {
+	
+	private static final Map<String, Country> countries = new HashMap<>();
+
+    @PostConstruct
+    public void initData() {
+        // initialize countries map
+    }
+
+    public Country findCountry(String name) {
+        return countries.get(name);
+    }
+
+}
