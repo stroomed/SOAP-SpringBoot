2255f06d5a68fe7ef330ec04e9d336c6106853e4
ricardo.gutierrez <ricardo.gutierrez.valejos@gmail.com>
2022-02-11 11:55:24 -0300

    Actualizacion

---
+++ SOAP/src/main/java/generated/ObjectFactory.java
@@ -0,0 +1,63 @@
+//
+// Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.3.2 
+// Visite <a href="https://javaee.github.io/jaxb-v2/">https://javaee.github.io/jaxb-v2/</a> 
+// Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
+// Generado el: 2022.02.11 a las 11:33:49 AM CLST 
+//
+
+
+package generated;
+
+import javax.xml.bind.annotation.XmlRegistry;
+
+
+/**
+ * This object contains factory methods for each 
+ * Java content interface and Java element interface 
+ * generated in the io.spring.guides.gs_producing_web_service package. 
+ * <p>An ObjectFactory allows you to programatically 
+ * construct new instances of the Java representation 
+ * for XML content. The Java representation of XML 
+ * content can consist of schema derived interfaces 
+ * and classes representing the binding of schema 
+ * type definitions, element declarations and model 
+ * groups.  Factory methods for each of these are 
+ * provided in this class.
+ * 
+ */
+@XmlRegistry
+public class ObjectFactory {
+
+
+    /**
+     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: io.spring.guides.gs_producing_web_service
+     * 
+     */
+    public ObjectFactory() {
+    }
+
+    /**
+     * Create an instance of {@link GetCountryRequest }
+     * 
+     */
+    public GetCountryRequest createGetCountryRequest() {
+        return new GetCountryRequest();
+    }
+
+    /**
+     * Create an instance of {@link GetCountryResponse }
+     * 
+     */
+    public GetCountryResponse createGetCountryResponse() {
+        return new GetCountryResponse();
+    }
+
+    /**
+     * Create an instance of {@link Country }
+     * 
+     */
+    public Country createCountry() {
+        return new Country();
+    }
+
+}
---
+++ SOAP/src/main/java/generated/package-info.java
@@ -0,0 +1,9 @@
+//
+// Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.3.2 
+// Visite <a href="https://javaee.github.io/jaxb-v2/">https://javaee.github.io/jaxb-v2/</a> 
+// Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
+// Generado el: 2022.02.11 a las 11:33:49 AM CLST 
+//
+
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://spring.io/guides/gs-producing-web-service", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package generated;
